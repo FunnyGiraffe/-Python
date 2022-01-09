3dc1d09d41ea303cada5a839e1cdf433b26d7c9e
FunnyGiraffe <1287767741@qq.com>
2022-01-09 23:35:07 +0800

    ch5_1_1

---
+++ ch5_1_1.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+'''
+@Time    :   2022/01/09 23:20:58
+@Author  :   Rao Guangxiang 
+@Version :   1.0
+@Contact :   raogx.vip@hotmail.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   None
+@File    :   ch5_1_1.py
+'''
+
+# here put the import lib
+i = 100
+while i < 1000:
+
+    op = i%10   # one place
+    tp = i//10 % 10     # ten place
+    hp = i//100 % 10    # hundreds place
+    if i == op**3 + tp**3 + hp**3:
+        print("%i是水仙花数！" %i)
+    i += 1
