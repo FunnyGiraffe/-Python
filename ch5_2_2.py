71db76dcc5439a73f5779f4326c66a1a07ef7938
FunnyGiraffe <1287767741@qq.com>
2022-01-10 15:25:55 +0800

    for循环

--- ch5_2_2.py
+++ ch5_2_2.py
@@ -5,8 +5,16 @@
 @Version :   1.0
 @Contact :   funnygiraffe@hotmail.com
 @License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
-@Desc    :   for
+@Desc    :   for 可迭代对象包括字符串、列表、元组、集合和字典
 @File    :   ch5_2_2.py
 '''
 
 # here put the import lib
+print("----字符串----")
+for item in 'Hello World':
+    print(item)
+
+numbers = [43, 32, 25, 64]
+print("----整数列表----")
+for item in numbers:
+    print(item)
