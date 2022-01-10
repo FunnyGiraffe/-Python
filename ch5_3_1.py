5176da7ea71b16aa7b1c8a17b657fa8c6f4d4ec1
FunnyGiraffe <1287767741@qq.com>
2022-01-10 16:32:31 +0800

    break continue

---
+++ ch5_3_1.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+'''
+@Time    :   2022/01/10 16:27:33
+@Author  :   funnygiraffe
+@Version :   1.0
+@Contact :   funnygiraffe@hotmail.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   break | continue
+@File    :   ch5_3_1.py
+'''
+
+# here put the import lib
+for item in range(10):
+    if item == 3:
+        #break
+        continue
+    print(item)
