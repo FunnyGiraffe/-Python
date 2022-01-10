77248b74096ed289c2a58ab33039aa6c71242a40
FunnyGiraffe <1287767741@qq.com>
2022-01-10 13:58:38 +0800

    while循环

---
+++ ch5_2_1.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+@Time    :   2022/01/10 13:55:54
+@Author  :   funnygiraffe
+@Version :   1.0
+@Contact :   funnygiraffe@hotmail.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   while
+@File    :   ch5_2_1.py
+'''
+
+# here put the import lib
+i = 0
+while i * i < 1000:
+    i += 1
+
+print("i = %d" %i)
+print("i * i = %d" %(i*i))
